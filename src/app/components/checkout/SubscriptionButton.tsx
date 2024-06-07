57a6eb6b09c30ba8f4cfc67e7ce10c6957b7530e
naokicode <kitajimanaoki.0917@icloud.com>
2024-06-08 01:27:59 +0900

    build

--- src/app/components/checkout/SubscriptionButton.tsx
+++ src/app/components/checkout/SubscriptionButton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from '@mui/material';
 import React from 'react';
 import { loadStripe } from "@stripe/stripe-js";
 
--- src/app/components/checkout/SubscriptionManagementButton.tsx
+++ src/app/components/checkout/SubscriptionManagementButton.tsx
@@ -1,6 +1,5 @@
 "use client";
 
-import { Button } from '@mui/material';
 import { useRouter } from 'next/navigation'; 
 import React from 'react';
 
